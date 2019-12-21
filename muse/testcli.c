94ef33864000ce7984695ce767aef80e55def531
karengrams <kmanrique@est.frba.utn.edu.ar>
2019-12-21 00:46:58 -0300

    En el asfalto hay...restos de sangre

--- muse/testcli.c
+++ muse/testcli.c
@@ -1,4 +1,33 @@
 #include "libmuse.h"
+
+void grabar_archivo(uint32_t arch, char* palabra)
+{
+	uint32_t offset;
+	//hilolay_wait(revolucion_recibida);
+	muse_get(&offset, arch, sizeof(uint32_t));
+	muse_cpy(arch + offset, palabra, strlen(palabra) + 1);
+	offset += strlen(palabra) + 1;
+	muse_cpy(arch, &offset, sizeof(uint32_t));
+	//hilolay_signal(revolucion_emitida);
+	sleep(1);
+}
+
+uint32_t leer_archivo(uint32_t arch, uint32_t leido)
+{
+	uint32_t offset;
+	char * palabra = malloc(100);
+	//hilolay_wait(presion_emitida);
+	muse_get(&offset, arch, sizeof(uint32_t));
+	uint32_t len = offset - leido;
+	muse_get(palabra, arch + leido, len);
+	offset += strlen(palabra) + 1;
+	muse_cpy(arch, &offset, sizeof(uint32_t));
+	//hilolay_signal(presion_recibida);
+	puts(palabra);
+	free(palabra);
+	return offset;
+}
+
 int main(){
 int *ptr;
 void* cosas = malloc(50);
@@ -6,19 +35,25 @@ void* recibir = malloc(265);
 void* recibir2 = malloc(265);
 int id = getpid();
 muse_init(id, "10.0.2.15", 9172);
-uint32_t my_memory = muse_alloc(100);
-//muse_cpy(50, "POR DIOS DONDE SORONGO ROMPEEEEEEE, SINCRO TE ODIO", 50); // Todo: para mi es aca donde muere
-uint32_t map = muse_map("stephen.txt",50,MAP_SHARED);
-/*
-	muse_cpy(map, "POR DIOS DONDE SORONGO ROMPEEEEEEE, SINCRO TE ODIO", 50); // Todo: para mi es aca donde muere
-	muse_get(recibir2,map,50);
-	muse_sync(map,10);
-	muse_unmap(map);
-	printf("my_memory=%lu\n",my_memory);
-	printf("map=%lu\n",map);
-	printf("muse_get(recibir2,map,50)=%s\n",(char*)recibir2);
-	muse_free(my_memory);
-	printf("POR DIOSOSSSSNKASNDKLASDFANF");*/
-muse_close();
+
+uint32_t arch = muse_map("archivito-loco-de-4096", 4096, MAP_SHARED);
+uint32_t offset = 0;
+
+offset = sizeof(uint32_t);
+for(int i = 0; i<6;i++)
+	offset = leer_archivo(arch, offset);
+
+offset = sizeof(uint32_t);
+muse_cpy(arch, &offset, sizeof(uint32_t));
+
+grabar_archivo(arch, "Es hora de levantarse y aprobar el TP\n");
+
+grabar_archivo(arch, "Es hora de aprobar y festejar\n");
+grabar_archivo(arch, "Saben que la entrega se esta por acabar\n");
+grabar_archivo(arch, "Asi que hagamoslo!\n");
+arch += 5000;
+muse_get(NULL, arch, 1);
+muse_sync(arch, 4096);
+muse_unmap(arch);
 return 0;
 }
